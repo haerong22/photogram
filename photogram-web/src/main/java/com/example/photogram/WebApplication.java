e6ad8f1bbe4188026c23e7cf1cf019c4c23d4f9d
haerong22 <haerong22@naver.com>
2021-06-10 14:48:58 +0900

    multi module

--- photogram-web/src/main/java/com/example/photogram/WebApplication.java
+++ photogram-web/src/main/java/com/example/photogram/WebApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PhotogramApplication {
-
+public class WebApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PhotogramApplication.class, args);
+        SpringApplication.run(WebApplication.class, args);
     }
-
 }
