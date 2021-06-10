1f3946becd931cf8287960af8f8dd2024ea2e77e
haerong22 <haerong22@naver.com>
2021-06-10 20:12:48 +0900

    modify build.gradle

--- photogram-web/src/main/java/com/photogram/web/controller/TestController.java
+++ photogram-web/src/main/java/com/photogram/web/controller/TestController.java
@@ -3,8 +3,11 @@ package com.photogram.web.controller;
 import com.photogram.core.domain.entity.Member;
 import com.photogram.core.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TestController {
@@ -17,5 +20,14 @@ public class TestController {
         Member member = new Member();
         memberRepository.save(member);
         return "success";
+    }
+
+    @GetMapping("member")
+    public List<Member> findAll() {
+        List<Member> all = memberRepository.findAll();
+        all.forEach(v -> {
+            System.out.println(v.getUserName());
+        });
+        return all;
     }
 }
