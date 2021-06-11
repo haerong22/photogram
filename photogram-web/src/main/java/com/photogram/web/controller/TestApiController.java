bf3da6add1ffca2ec103ce8a18324d863d295fcb
haerong22 <haerong22@naver.com>
2021-06-11 12:09:39 +0900

    add thymeleaf

---
+++ photogram-web/src/main/java/com/photogram/web/controller/TestApiController.java
@@ -0,0 +1,33 @@
+package com.photogram.web.controller;
+
+import com.photogram.core.domain.entity.Member;
+import com.photogram.core.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TestApiController {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @PostMapping("/member")
+    public String saveMember() {
+        Member member = new Member();
+        memberRepository.save(member);
+        return "success";
+    }
+
+    @GetMapping("member")
+    public List<Member> findAll() {
+        List<Member> all = memberRepository.findAll();
+        all.forEach(v -> {
+            System.out.println(v.getUserName());
+        });
+        return all;
+    }
+}
--- photogram-web/src/main/java/com/photogram/web/controller/TestController.java
+++ photogram-web/src/main/java/com/photogram/web/controller/TestController.java
@@ -1,33 +1,16 @@
 package com.photogram.web.controller;
 
-import com.photogram.core.domain.entity.Member;
-import com.photogram.core.repository.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.List;
-
-@RestController
+@Controller
 public class TestController {
 
-    @Autowired
-    private MemberRepository memberRepository;
-
-    @PostMapping("/member")
-    public String saveMember() {
-        Member member = new Member();
-        memberRepository.save(member);
-        return "success";
-    }
-
-    @GetMapping("member")
-    public List<Member> findAll() {
-        List<Member> all = memberRepository.findAll();
-        all.forEach(v -> {
-            System.out.println(v.getUserName());
-        });
-        return all;
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String hello(Model model) {
+        model.addAttribute("hello", "하이하이");
+        return "/test";
     }
 }
