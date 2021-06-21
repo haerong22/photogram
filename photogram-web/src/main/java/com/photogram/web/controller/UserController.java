6afada25c9f048800e15f0cb626427a268e1e80a
haerong22 <haerong22@naver.com>
2021-06-21 19:00:42 +0900

    thymeleaf extras

--- photogram-web/src/main/java/com/photogram/web/controller/UserController.java
+++ photogram-web/src/main/java/com/photogram/web/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.photogram.web.controller;
 
+import com.photogram.web.config.auth.PrincipalDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -13,7 +16,10 @@ public class UserController {
     }
 
     @GetMapping("/user/{id}/update")
-    public String update(@PathVariable long id) {
+    public String update(@PathVariable long id,
+                         @AuthenticationPrincipal PrincipalDetails principalDetails,
+                         Model model) {
+        model.addAttribute("information", principalDetails);
         return "user/update";
     }
 }
