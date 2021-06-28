e7692818a440e78625fee8b5d9b772770a0fef41
haerong22 <haerong22@naver.com>
2021-06-28 19:48:26 +0900

    subscribe modal api

--- photogram-web/src/main/resources/static/js/profile.js
+++ photogram-web/src/main/resources/static/js/profile.js
@@ -2,12 +2,11 @@
   1. 유저 프로파일 페이지
   (1) 유저 프로파일 페이지 구독하기, 구독취소
   (2) 구독자 정보 모달 보기
-  (3) 구독자 정보 모달에서 구독하기, 구독취소
-  (4) 유저 프로필 사진 변경
-  (5) 사용자 정보 메뉴 열기 닫기
-  (6) 사용자 정보(회원정보, 로그아웃, 닫기) 모달
-  (7) 사용자 프로파일 이미지 메뉴(사진업로드, 취소) 모달 
-  (8) 구독자 정보 모달 닫기
+  (3) 유저 프로필 사진 변경
+  (4) 사용자 정보 메뉴 열기 닫기
+  (5) 사용자 정보(회원정보, 로그아웃, 닫기) 모달
+  (6) 사용자 프로파일 이미지 메뉴(사진업로드, 취소) 모달
+  (7) 구독자 정보 모달 닫기
  */
 
 // (1) 유저 프로파일 페이지 구독하기, 구독취소
@@ -69,9 +68,9 @@ function getSubscribeModalItem(user) {
 			<div class="subscribe__btn">`;
 			if(!user.equalUserState) {
 				if (user.subscribeState) {
-					item += `<button class="cta blue" onClick="toggleSubscribeModal(this)">구독취소</button>`;
+					item += `<button class="cta blue" onClick="toggleSubscribe(${user.id}, this)">구독취소</button>`;
 				} else {
-					item += `<button class="cta blue" onClick="toggleSubscribeModal(this)">구독하기</button>`;
+					item += `<button class="cta blue" onClick="toggleSubscribe(${user.id}, this)">구독하기</button>`;
 				}
 			}
 			item += `
@@ -81,19 +80,7 @@ function getSubscribeModalItem(user) {
 	return item;
 }
 
-
-// (3) 구독자 정보 모달에서 구독하기, 구독취소
-function toggleSubscribeModal(obj) {
-	if ($(obj).text() === "구독취소") {
-		$(obj).text("구독하기");
-		$(obj).toggleClass("blue");
-	} else {
-		$(obj).text("구독취소");
-		$(obj).toggleClass("blue");
-	}
-}
-
-// (4) 유저 프로파일 사진 변경 (완)
+// (3) 유저 프로파일 사진 변경 (완)
 function profileImageUpload() {
 	$("#userProfileImageInput").click();
 
@@ -115,7 +102,7 @@ function profileImageUpload() {
 }
 
 
-// (5) 사용자 정보 메뉴 열기 닫기
+// (4) 사용자 정보 메뉴 열기 닫기
 function popup(obj) {
 	$(obj).css("display", "flex");
 }
@@ -125,17 +112,17 @@ function closePopup(obj) {
 }
 
 
-// (6) 사용자 정보(회원정보, 로그아웃, 닫기) 모달
+// (5) 사용자 정보(회원정보, 로그아웃, 닫기) 모달
 function modalInfo() {
 	$(".modal-info").css("display", "none");
 }
 
-// (7) 사용자 프로파일 이미지 메뉴(사진업로드, 취소) 모달
+// (6) 사용자 프로파일 이미지 메뉴(사진업로드, 취소) 모달
 function modalImage() {
 	$(".modal-image").css("display", "none");
 }
 
-// (8) 구독자 정보 모달 닫기
+// (7) 구독자 정보 모달 닫기
 function modalClose() {
 	$(".modal-subscribe").css("display", "none");
 	location.reload();
