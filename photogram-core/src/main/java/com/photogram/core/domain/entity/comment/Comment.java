1f043f5dc6a24388259f82b4f67f0c0bc524d377
haerong22 <haerong22@naver.com>
2021-06-30 22:37:18 +0900

    comment entity

---
+++ photogram-core/src/main/java/com/photogram/core/domain/entity/comment/Comment.java
@@ -0,0 +1,41 @@
+package com.photogram.core.domain.entity.comment;
+
+import com.photogram.core.domain.entity.image.Image;
+import com.photogram.core.domain.entity.user.User;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String content;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne
+    private User user;
+
+    @JoinColumn(name = "image_id")
+    @ManyToOne
+    private Image image;
+
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
+
+    @PrePersist
+    public void createDate() {
+        this.createDate = LocalDateTime.now();
+    }
+}
---
+++ photogram-core/src/main/java/com/photogram/core/repository/CommentRepository.java
@@ -0,0 +1,10 @@
+package com.photogram.core.repository;
+
+import com.photogram.core.domain.entity.comment.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+}
